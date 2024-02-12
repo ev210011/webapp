385d6a76aebd398e9c10d0f49082148329f58f82
ev210011 <85009260+ev210011@users.noreply.github.com>
2024-02-12 14:17:35 +0900

    Update settings.py

--- pj_login/settings.py
+++ pj_login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-eg%o(d2&#%#jy!p$%yostwsxq^6j(*tcr!4fn4vg%@3rwve%s9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
