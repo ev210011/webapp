2d6e7ac96b369b25505e174844d4044dc6e38006
ev210011 <85009260+ev210011@users.noreply.github.com>
2024-02-12 13:22:27 +0900

    Update settings.py

--- pj_login/settings.py
+++ pj_login/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "pj_login.wsgi.application"
 default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
 
 DATEBASES = {
-    "default":config("DATABASE_URL", default=default_dburl, cast=dburl)
+    "default":config("DATABASE_URL", default=default_dburl, cast=dburl),
 }
 
 
