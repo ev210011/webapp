eb99cc5739902d681ba173a4d4696eb0fe82cc8d
ev210011 <85009260+ev210011@users.noreply.github.com>
2024-02-12 13:04:38 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py superuser
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
+python3 manage.py superuser
