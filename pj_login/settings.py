33fa6f0c523e0ec4aac3a77c2e3ef8eaf7e097a8
ev210011 <85009260+ev210011@users.noreply.github.com>
2024-02-12 13:41:14 +0900

    Update settings.py

--- pj_login/settings.py
+++ pj_login/settings.py
@@ -89,8 +89,8 @@ default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
 DATEBASES = {
     #"default":config("DATABASE_URL", default=default_dburl, cast=dburl),
     'default':{
-        'ENGINE':'django.db.backends.sqlite3',
-        #'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.django_renser_db',
+        'NAME':django_render_db,
     }
 }
 
