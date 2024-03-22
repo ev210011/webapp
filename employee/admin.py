a7f75aca61f37667ceaa8baad663437e6e78099b
ev210011 <85009260+ev210011@users.noreply.github.com>
2024-03-22 10:50:30 +0900

    Update admin.py

--- employee/admin.py
+++ employee/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Employee, Workplace
+from .models import Employee, Number, Workplace
 
 
 admin.site.register(Employee)
+admin.site.register(Number)
 admin.site.register(Workplace)
