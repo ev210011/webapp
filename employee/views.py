544454bcf1f03362cdccfad59bb165e8cbf42279
ev210011 <85009260+ev210011@users.noreply.github.com>
2024-03-22 10:51:26 +0900

    Update views.py

--- employee/views.py
+++ employee/views.py
@@ -24,17 +24,77 @@ class IndexView(generic.ListView):
         if workplace:
             queryset = queryset.filter(workplace=workplace)
 
-        id = form.cleaned_data['id']
-        if id:
-            queryset = queryset.filter(id=id)
+        number = form.cleaned_data['number']
+        if number:
+            queryset = queryset.filter(number=number)
+       
+        full_name = form.cleaned_data['full_name']
+        if full_name:
+            queryset = queryset.filter(full_name=full_name)
+        return queryset
+        
 
+'''
         name = form.cleaned_data['name']
         if name:
-            queryset = Employee.objects.annotate(name=Concat('last_name', 'first_name')).filter(
+            queryset = Employee.objects.annotate(full_name=Concat('last_name', 'first_name')).filter(
                 Q(last_name__icontains=name) &
-                Q(first_name__icontains=name)
+                Q(first_name__icontains=name) &
+                Q(full_name__icontains=name)
             ).order_by('pk').distinct()
         else:
-            name = Employee.objects.all().order_by('pk')
+            queryset = Employee.objects.all().order_by('pk')
+        
         return queryset
+
+
+
+class EmployeeListView(generic.ListView):
+    model = Employee
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['form'] = SearchForm(self.request.GET)
+        return context
     
+    def get_queryset(self):
+        form = SearchForm(self.request.GET)
+        form.is_valid()
+
+        name = self.request.GET.get('queryset')
+
+        if name:
+            name = name.replace(' ', '')
+            name = name.replace(' ', '')
+            queryset = Employee.objects.annotate(full_name=Concat('last_name', 'first_name')).filter(
+                Q(last_name__icontains=name) &
+                Q(first_name__icontains=name) &
+                Q(full_name__icontains=name)
+            ).order_by('pk').distinct()
+        else:
+            queryset = Employee.objects.all().order_by('pk')
+        return queryset
+
+
+
+class EmployeeListView(generic.ListView):
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['form'] = SearchForm(self.request.GET)
+        return context
+    
+    def get_queryset(self):
+        form = SearchForm(self.request.GET)
+        form.is_valid()
+
+        name = self.request.GET.get('queryset')
+
+        if name:
+            queryset = queryset.filter(name=name)
+        return queryset.annotate(
+            name = Concat('last_name', 'first_name')).filter(
+                Q(last_name__icontains=name) &
+                Q(first_name__icontains=name) &
+                Q(full_name__icontains=name)
+            ).order_by('pk').distinct()
+'''
