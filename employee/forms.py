6ed765bb0f7d1090c119b6d5c0d6485e5a437cc7
ev210011 <85009260+ev210011@users.noreply.github.com>
2024-03-22 10:50:03 +0900

    Update forms.py

--- employee/forms.py
+++ employee/forms.py
@@ -1,14 +1,14 @@
 from django import forms
-from .models import Employee, Workplace
+from .models import Employee, Number, Workplace
+from django.db.models.functions import Concat
 
 
 class SearchForm(forms.Form):
-    name = forms.ModelChoiceField(
-        queryset = Employee.objects, label = '氏名', required = False)
-    
-    id = forms.ModelChoiceField(
-        queryset = Employee.objects, to_field_name='id', label = '従業員番号', required = False)
+    full_name = forms.ModelChoiceField(
+        queryset = Employee.objects, label='氏名', required=False)
+       
+    number = forms.ModelChoiceField(
+        queryset = Number.objects, label='従業員番号', required=False)
 
     workplace = forms.ModelChoiceField(
-        queryset=Workplace.objects, label='勤務地', required=False)
-    
+        queryset = Workplace.objects, label='勤務地', required=False)
